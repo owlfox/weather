4bf1488057aba9cb5ec7b6c4fccd2ca1591f2429
Michael, Ze-Ren Chen <tsejen.tw@gmail.com>
2019-03-11 18:28:19 +1030

    Add line chart view of weather data

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'weather/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'weather/data', :defaults => { :format => 'json' }
 end
